cb7f293e98fc4f47497819a7c54aca71dc416aaf
= <=>
2024-04-27 16:35:06 +0500

    first commit

---
+++ src/components/TourList.jsx
@@ -0,0 +1,19 @@
+import Tour from "./Tour";
+
+export default function TourList({ tours, handleDelete }) {
+  // const renderedTours = tours.map((tour) => (
+  //   <Tour
+  //     {...tour}
+  //     key={tour.id}
+  //     handleDelete={handleDelete}
+  //   />
+  // ));
+
+  return (
+    <div className="mt-12 tours-list">
+      {tours.map((tour) => {
+        return <Tour {...tour} key={tour.id} handleDelete={handleDelete} />
+      })}
+    </div>
+  );
+}
